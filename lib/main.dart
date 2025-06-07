27936157bce8e6cb3e1c2fea957c7b8065abde86
heydi20 <heydipantoja1@gmail.com>
2025-06-07 16:53:25 -0500

    reproductor

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_final/screens/ReproduccionScreen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProyectoF());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ProyectoF extends StatelessWidget {
+  const ProyectoF({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return const MaterialApp(home: Cuerpo());
+  }
+}
+
+class Cuerpo extends StatelessWidget {
+  const Cuerpo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pantalla de Inicio')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ReproduccionScreen(),
+              ),
+            );
+          },
+          child: const Text('Ir a Reproductor'),
         ),
       ),
     );
